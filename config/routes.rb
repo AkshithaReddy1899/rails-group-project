bb126bf795931cccfbcc3d069fef22f5621713c0
Ola Ishola <olaishola@hotmail.co.uk>
2022-05-16 17:20:03 +0100

    Populate the recipe controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :users do
-    resources :foods
-    resources :recipes
+    resources :recipes, only: %i[index new create destroy show]
+    resources :foods, only: %i[index new create destroy]
   end
 
   root "users#index"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+user_one = User.create(name: "Reddy")
+user_two = User.create(name: "Ola")
+user_three = User.create(name: "Kalle")
+
+
+shawama = Recipe.create(name: 'Shawarma', preparation_time: '5 hours', cooking_time: '2 hours', description: 'Very delicious', public: true, user_id: 1)
+jollof = Recipe.create(name: 'Jollof Rice', preparation_time: '3 hours', cooking_time: '1.3 hours', description: 'Very delicious', public: true, user_id: 2)
+chicken_curry = Recipe.create(name: 'Chicken Curry', preparation_time: '3 hours', cooking_time: '3 hours', description: 'Very delicious and taste so good', public: false, user_id: 3)
