66035678a92156239817c769f14579673e50b6d4
Akshitha Reddy <akshithareddy1899@gmail.com>
2022-05-20 20:50:25 +0530

    Fix styling

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -12,7 +12,7 @@ class Ability
       can :manage, [Food, Recipe], user_id: user.id
       can :read, [Food, Recipe]
       can :create, [Food, Recipe]
-      can :destroy, [Food, Recipe], user_id: user.id
+      can :destroy, [Food, Recipe, RecipeFood]
     end
     #
     # The first argument to `can` is the action you are giving the user
