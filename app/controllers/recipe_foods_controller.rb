5069c557669b48cb166b3d283a4849a8e56b8abf
Akshitha Reddy <akshithareddy1899@gmail.com>
2022-05-21 19:16:15 +0530

    Remove unused code

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,29 +1,20 @@
 class RecipeFoodsController < ApplicationController
-  # before_action :set_recipe_food, only: %i[show edit update destroy]
-
-  # GET /recipe_foods or /recipe_foods.json
   def index
     @recipe_foods = RecipeFood.all
   end
 
-  # GET /recipe_foods/1 or /recipe_foods/1.json
   def show; end
 
-  # GET /recipe_foods/new
   def new
-    # @recipe = current_user.recipes.id
     @foods = Food.all
     @recipe_food = RecipeFood.new
-    # @foods = Food.all.select { |food| @recipe.foods.exclude?(food) }
   end
 
-  # GET /recipe_foods/1/edit
   def edit
     @recipe_food = RecipeFood.find(params[:id])
     @foods = Food.all
   end
 
-  # POST /recipe_foods or /recipe_foods.json
   def create
     @recipe_food = current_user.recipes.find(params[:recipe_id]).recipe_foods.new(recipe_food_params)
 
@@ -38,7 +29,6 @@ class RecipeFoodsController < ApplicationController
     end
   end
 
-  # DELETE /recipe_foods/1 or /recipe_foods/1.json
   def destroy
     @recipe_food = RecipeFood.find(params[:id])
     redirect_to recipe_path(params[:recipe_id]) if @recipe_food.destroy
@@ -46,12 +36,6 @@ class RecipeFoodsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  # def set_recipe_food
-  # @recipe_food = RecipeFood.find(params[:id])
-  # end
-
-  # Only allow a list of trusted parameters through.
   def recipe_food_params
     params.require(:recipe_food).permit(:quantity, :food_id)
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,26 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-user_one = User.create(name: "Reddy")
-user_two = User.create(name: "Ola")
-user_three = User.create(name: "Kalle")
-
-
-shawama = Recipe.create(
-    name: 'Shawarma',
-    preparation_time: '5 hours',
-    cooking_time: '2 hours',
-    description: 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs',
-    # public: true,
-    user_id: user_one.id
-)
-
-jollof = Recipe.create(
-    name: 'Jollof Rice',
-    preparation_time: '3 hours',
-    cooking_time: '1.3 hours',
-    description: 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs',
-    public: true,
-    user_id: user_two.id
-)
 
