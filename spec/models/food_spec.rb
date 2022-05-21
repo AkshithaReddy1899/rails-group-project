6f89577959412be9aa6a2cf5a711375f0bb4e155
Akshitha Reddy <akshithareddy1899@gmail.com>
2022-05-21 10:52:55 +0530

    Test for recipe_foods

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,18 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  subject do
-    described_class.new(name: 'Apple', measurement_unit: 20, price: 1, user_id: 1)
+  before do
+    @food = Food.new(name: 'Apple', measurement_unit: 20, price: 1, user_id: 1)
   end
-  before { subject.save }
-
-  it 'Food name should not be null' do
-    subject.name = nil
-    expect(subject).to_not be_valid
+  it 'name should not be null' do
+    @food.name = nil
+    expect(@food).to_not be_valid
   end
 
-  it 'Food measurement unit should not be null' do
-    subject.measurement_unit = nil
-    expect(subject).to_not be_valid
+  it 'measurement unit should not be null' do
+		@food.measurement_unit = nil
+    expect(@food).to_not be_valid
+  end
+
+	it 'measurement unit should be greater than zero' do
+		expect(@food.measurement_unit).to be >= 1
+  end
+
+	it 'price should not be null' do
+		@food.price = nil
+    expect(@food).to_not be_valid
+  end
+
+	it 'price should be greater than zero' do
+		expect(@food.price).to be >= 1
   end
 end
